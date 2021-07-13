0c4b1ab63f65b29002e0c0912f3cf065e964c562
JanotLeLapin <51287094+JanotLeLapin@users.noreply.github.com>
2021-07-13 18:38:20 +0200

    Draw sample red cube

--- src/App.svelte
+++ src/App.svelte
@@ -1,65 +1,54 @@
 <script lang="ts">
-  import logo from './assets/svelte.png'
-  import Counter from './lib/Counter.svelte'
+  import { onMount } from 'svelte';
+
+  import * as THREE from 'three';
+
+  let el;
+
+  onMount(() => {
+    const scene = new THREE.Scene();
+    const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+    const geometry = new THREE.BoxGeometry();
+    const material = new THREE.MeshBasicMaterial({ color: 0xff0000, wireframe: true });
+    const cube = new THREE.Mesh(geometry, material);
+    let renderer;
+    scene.add(cube);
+    camera.position.z = 30;
+    camera.position.y = -0.25;
+
+    const animate = () => {
+      requestAnimationFrame(animate);
+      cube.rotation.y += 0.01;
+      camera.position.z = camera.position.z < -10 ? 30 : camera.position.z - 0.1;
+      renderer.render(scene, camera);
+    };
+
+    const resize = () => {
+      renderer.setSize(window.innerWidth, window.innerHeight)
+      camera.aspect = window.innerWidth / window.innerHeight;
+      camera.updateProjectionMatrix();
+    };
+
+    const createScene = (el) => {
+      renderer = new THREE.WebGLRenderer({ antialias: true, canvas: el });
+      resize();
+      animate();
+    }
+
+    createScene(el);
+
+    window.addEventListener('resize', resize);
+  });
 </script>
 
 <main>
-  <img src={logo} alt="Svelte Logo" />
-  <h1>Hello Typescript!</h1>
-
-  <Counter />
-
-  <p>
-    Visit <a href="https://svelte.dev">svelte.dev</a> to learn how to build Svelte
-    apps.
-  </p>
-
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme">SvelteKit</a> for
-    the officially supported framework, also powered by Vite!
-  </p>
+  <canvas bind:this={el}></canvas>
 </main>
 
 <style>
-  :root {
-    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen,
-      Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
-  }
-
-  main {
-    text-align: center;
-    padding: 1em;
-    margin: 0 auto;
-  }
-
-  img {
-    height: 16rem;
-    width: 16rem;
-  }
-
-  h1 {
-    color: #ff3e00;
-    text-transform: uppercase;
-    font-size: 4rem;
-    font-weight: 100;
-    line-height: 1.1;
-    margin: 2rem auto;
-    max-width: 14rem;
-  }
-
-  p {
-    max-width: 14rem;
-    margin: 1rem auto;
-    line-height: 1.35;
-  }
-
-  @media (min-width: 480px) {
-    h1 {
-      max-width: none;
-    }
-
-    p {
-      max-width: none;
-    }
+  canvas {
+    position: fixed;
+    top: 0;
+    left: 0;
   }
 </style>
