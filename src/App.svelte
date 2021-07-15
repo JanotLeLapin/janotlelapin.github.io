3909988d4762b1c61141a3f9fd7641cab976eb1c
JanotLeLapin <51287094+JanotLeLapin@users.noreply.github.com>
2021-07-15 16:11:03 +0200

    Updates

--- src/App.svelte
+++ src/App.svelte
@@ -69,29 +69,40 @@
 <main>
   <canvas bind:this={el}></canvas>
   <div class="content">
-    <header>
+    <div class="container">
+      <header>
+        <h1>JanotLeLapin</h1>
+        <h3>Bienvenue 👋</h3>
+      </header>
+      <section>
+        <h3>📚 Mes compétences</h3>
+        <p> - Java (API de Spigot)</p>
+        <p> - Next.js</p>
+        <p> - Discord.js</p>
+        <p> - Angular</p>
+        <p> - React</p>
+        <p> - Svelte</p>
+      </section>
+      <section>
+        <h3>🏆 Mon travail</h3>
+        <p> - AC UHC</p>
+      </section>
+      <section class="wide">
+        <h3>☎️ Me contacter</h3>
+        <p>Vous avez des idées ? Super, je peux les développer !</p>
+        <p>Contactez moi sur Discord : <a target="_blank" href="https://discord.com/users/437953881914474523">JanotLeLapin#4989</a>.</p>
+        <p>Le prix dépendra de la complexité du projet.</p>
+      </section>
+    </div>
+    <footer>
       <h1>JanotLeLapin</h1>
-      <h3>Bienvenue 👋</h3>
-    </header>
-    <section>
-      <h3>📚 Mes compétences</h3>
-      <p> - Java (API de Spigot)</p>
-      <p> - Next.js</p>
-      <p> - Discord.js</p>
-      <p> - Angular</p>
-      <p> - React</p>
-      <p> - Svelte</p>
-    </section>
-    <section>
-      <h3>🏆 Mon travail</h3>
-      <p> - AC UHC</p>
-    </section>
-    <section class="wide">
-      <h3>☎️ Me contacter</h3>
-      <p>Vous avez des idées ? Super, je peux les développer !</p>
-      <p>Contactez moi sur Discord : <a href="https://discord.com/users/437953881914474523">JanotLeLapin#4989</a>.</p>
-      <p>Le prix dépendra de la complexité du projet.</p>
-    </section>
+      <section>
+        <h3>Liens</h3>
+        <p><a target="_blank" href="https://discord.com/users/437953881914474523">Discord</a></p>
+        <p><a target="_blank" href="https://github.com/JanotLeLapin">GitHub</a></p>
+        <p><a target="_blank" href="https://www.youtube.com/channel/UC8zxDVecLTSVLpO88Z6KvcA">YouTube</a></p>
+      </section>
+    </footer>
   </div>
 </main>
 
@@ -130,16 +141,22 @@
   }
 
   .content {
-    display: grid;
-    grid-template-columns: 2rem repeat(3, 1fr) 2rem;
-    grid-row-gap: 8rem;
+    display: flex;
+    flex-direction: column;
     width: 100%;
+    height: 100%;
     position: absolute;
+  }
+
+  .container {
+    flex: 1 0 auto;
     margin: 2rem 0;
 
-    > * {
-      grid-column-start: 2;
+    p {
+      text-align: justify;
+    }
 
+    > * {
       box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
       background-color: rgba(0, 0, 0, 0.75);
       border-radius: .5rem;
@@ -148,19 +165,59 @@
     }
   }
 
-  p {
-    text-align: justify;
-  }
-
-  header {
-    grid-column: 2 / 5;
-  }
-
-  .wide {
-    grid-column: 2 / 4;
-  }
-
   h1, h3 {
     color: var(--fg-1);
+  }
+
+  footer {
+    display: flex;
+    flex-shrink: 0;
+    background-color: var(--bg-2);
+    // clip-path: polygon(0 0, 50% 2rem, 100% 0, 100% 100%, 0px 100%);
+    padding: 2rem;
+  }
+
+  @media only screen and (min-width: 600px) {
+    .container {
+      display: grid;
+      grid-template-columns: 2rem repeat(3, 1fr) 2rem;
+      grid-row-gap: 8rem;
+
+      > * {
+        grid-column-start: 2;
+      }
+    }
+
+    header {
+      grid-column: 2 / 5;
+    }
+
+    .wide {
+      grid-column: 2 / 4;
+    }
+
+    footer {
+      align-items: center;
+      padding: 2rem 4rem;
+
+      > h1 {
+        padding-right: 4rem;
+      }
+    }
+  }
+
+  @media only screen and (max-width: 600px) {
+    .container > * {
+      margin: 2rem;
+    }
+
+    footer {
+      flex-direction: column;
+      text-align: center;
+
+      > section {
+        text-align: center;
+      }
+    }
   }
 </style>
