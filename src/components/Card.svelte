989c9bf6319fa8727450dddbaea7c8817eeebc52
Joseph <joseph300905@gmail.com>
2022-11-05 17:36:42 +0100

    Make card a link to the repo

--- src/components/Card.svelte
+++ src/components/Card.svelte
@@ -4,15 +4,17 @@
   export let repo: Repo;
 </script>
 
-<div class="relative cursor-pointer border border-gray-200 p-4 h-full shadow rounded hover:-translate-y-1 hover:scale-[101%] hover:shadow-lg transition-all">
-  <div class="mb-6">
-    <p class="text-xl">{repo.owner}/<span class="font-bold text-gray-900">{repo.repo}</span></p>
-    {#if repo.description}
-      <p>{repo.description}</p>
-    {/if}
+<a href={`https://github.com/${repo.owner}/${repo.repo}`} target="_blank" rel="noreferrer">
+  <div class="relative cursor-pointer border border-gray-200 p-4 h-full shadow rounded hover:-translate-y-1 hover:scale-[101%] hover:shadow-lg transition-all">
+    <div class="mb-6">
+      <p class="text-xl">{repo.owner}/<span class="font-bold text-gray-900">{repo.repo}</span></p>
+      {#if repo.description}
+        <p>{repo.description}</p>
+      {/if}
+    </div>
+    <p class="absolute bottom-4 right-4 lang font-semibold" style={`--lang-color: ${repo.languageColor}`}>{repo.language}</p>
   </div>
-  <p class="absolute bottom-4 right-4 lang font-semibold" style={`--lang-color: ${repo.languageColor}`}>{repo.language}</p>
-</div>
+</a>
 
 <style>
   .lang {
