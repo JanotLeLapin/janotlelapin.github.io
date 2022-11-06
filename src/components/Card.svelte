6f857092cac84adc18058f31a6aae3e5049d32c5
Joseph <joseph300905@gmail.com>
2022-11-06 10:58:58 +0100

    Complex styling

--- src/components/Card.svelte
+++ src/components/Card.svelte
@@ -8,9 +8,7 @@
   <div class="relative cursor-pointer border border-gray-200 p-4 h-full shadow rounded hover:-translate-y-1 hover:scale-[101%] hover:shadow-lg transition-all">
     <div class="mb-6">
       <p class="text-xl">{repo.owner}/<span class="font-bold text-gray-900">{repo.repo}</span></p>
-      {#if repo.description}
-        <p>{repo.description}</p>
-      {/if}
+      {#if repo.description}<p>{repo.description}</p>{/if}
     </div>
     <p class="absolute bottom-4 right-4 lang font-semibold" style={`--lang-color: ${repo.languageColor}`}>{repo.language}</p>
   </div>
