3793c531b03a31421fa73d66b1566f40e3656e4e
Joseph <joseph300905@gmail.com>
2022-11-06 18:27:26 +0100

    Those are some pretty waves

--- src/components/Card.svelte
+++ src/components/Card.svelte
@@ -5,7 +5,7 @@
 </script>
 
 <a href={`https://github.com/${repo.owner}/${repo.repo}`} target="_blank" rel="noreferrer">
-  <div class="relative cursor-pointer text-gray-700 border border-gray-200 p-4 h-full shadow rounded hover:-translate-y-1 hover:scale-[101%] hover:shadow-lg transition-all">
+  <div class="relative cursor-pointer text-left text-gray-700 border border-gray-200 p-4 h-full shadow rounded hover:-translate-y-1 hover:scale-[101%] hover:shadow-lg transition-all">
     <div class="mb-6">
       <p class="text-xl">{repo.owner}/<span class="font-bold text-gray-900">{repo.repo}</span></p>
       {#if repo.description}<p>{repo.description}</p>{/if}
