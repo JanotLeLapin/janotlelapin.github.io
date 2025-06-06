e925f9857990e3df36e8ed1a2d761210742978f8
Joseph DALY <joseph300905@gmail.com>
2025-06-06 13:30:43 +0200

    human can play against ai

--- src/components/Chess.svelte
+++ src/components/Chess.svelte
@@ -3,34 +3,104 @@
 
   import * as poulet from '../wasm'
 
-  let board: poulet.PouletPiece[] = $state(Array(64).fill(null));
-  let latestMove: poulet.PouletMove | null = $state(null);
+  type Game = {
+    game: poulet.PouletGame;
+    controller: poulet.PouletNetwork | poulet.PouletNetwork[];
+  };
 
-  let game: poulet.PouletGame | null = $state(null);
-  let brains: poulet.PouletNetwork[] | null = $state(null);
+  type Position = {
+    x: number,
+    y: number,
+  };
+
+  type Move = {
+    src: Position,
+    dst: Position,
+  };
+
+  let board: poulet.PouletPiece[] = $state(Array(64).fill(null));
+  let latestMove: Move | null = $state(null);
+
+  let game: Game | null = $state(null);
+
+  let click: Position | null = $state(null);
 
   onMount(async () => {
     const network = await fetch('/chess/brain.model').then(res => res.arrayBuffer())
     await poulet.default();
-    game = poulet.PouletGame.new();
-    board = game.get_board();
-    brains = [poulet.PouletNetwork.load(new Uint8Array(network)) as poulet.PouletNetwork, poulet.PouletNetwork.init() as poulet.PouletNetwork];
+
+    const smartNetwork = poulet.PouletNetwork.load(new Uint8Array(network));
+    if (!smartNetwork) {
+      console.error('could not load smart neural network');
+      return;
+    }
+
+    game = {
+      game: poulet.PouletGame.new(),
+      controller: smartNetwork,
+    };
+    board = game.game.get_board();
   })
 
   const next = () => {
-    if (!brains || !game) {
+    if (!game) {
       return;
     }
 
-    const move = (game.get_turn() == poulet.PouletColor.from_string("white") ? brains[0] : brains[1]).next_move(game, 1.0);
-    if (!move) {
-      console.log('end of game!');
+    if (Array.isArray(game.controller)) {
+      const move = (game.game.get_turn().to_string() == 'white' ? game.controller[0] : game.controller[1]).next_move(game.game, game.game.get_turn().to_string() == 'white' ? 0.1 : 1.0);
+      if (!move) {
+        console.log('end of game!');
+        return;
+      }
+
+      game.game.do_move(move.get_src_x(), move.get_src_y(), move.get_dst_x(), move.get_dst_y());
+      latestMove = {
+        src: { x: move.get_src_x(), y: move.get_src_y() },
+        dst: { x: move.get_dst_x(), y: move.get_dst_y() },
+      };
+      board = game.game.get_board();
+    } else {
+      if (game.game.get_turn().to_string() !== 'white') {
+        return;
+      }
+
+      const move = game.controller.next_move(game.game, 1.0);
+      if (!move) {
+        console.log('end of game!');
+        return;
+      }
+
+      game.game.do_move(move.get_src_x(), move.get_src_y(), move.get_dst_x(), move.get_dst_y());
+      latestMove = {
+        src: { x: move.get_src_x(), y: move.get_src_y() },
+        dst: { x: move.get_dst_x(), y: move.get_dst_y() },
+      };
+      board = game.game.get_board();
+    }
+  }
+
+  const clickSquare = (x: number, y: number) => {
+    if (!game) {
       return;
     }
 
-    game.do_move(move.get_src_x(), move.get_src_y(), move.get_dst_x(), move.get_dst_y());
-    latestMove = move;
-    board = game.get_board();
+    if (click == null) {
+      click = { x, y }
+    } else {
+      if (!game.game.is_move_safe(click.x, click.y, x, y)) {
+        click = null;
+        return;
+      }
+
+      game.game.do_move(click.x, click.y, x, y);
+      latestMove = {
+        src: { x: click.x, y: click.y },
+        dst: { x, y },
+      }
+      board = game.game.get_board();
+      click = null;
+    }
   }
 </script>
 
@@ -38,11 +108,11 @@
   <h1>Chess!</h1>
   <div class="board">
     {#each board as piece, i}
-      <div class="square {((i + Math.floor(i / 8)) % 2 === 0) ? 'light' : 'dark'} {latestMove && i == latestMove.get_dst_x() + latestMove.get_dst_y() * 8 ? 'dst' : ''} {latestMove && i == latestMove.get_src_x() + latestMove.get_src_y() * 8 ? 'src' : ''}">
+      <button onclick={() => clickSquare(i % 8, Math.floor(i / 8))} class="square {((i + Math.floor(i / 8)) % 2 === 0) ? 'light' : 'dark'} {latestMove && i == latestMove.dst.x + latestMove.dst.y * 8 ? 'dst' : ''} {latestMove && i == latestMove.src.x + latestMove.src.y * 8 ? 'src' : ''} {click && i == click.x + click.y * 8 ? 'click' : ''}">
         {#if piece?.is_some()}
           <img src="/chess/{piece.get_color()?.to_string()}-{piece.get_piece_type()?.to_string()}.svg" alt="{piece.get_color()?.to_string()} and {piece.get_piece_type()?.to_string()}" />
         {/if}
-      </div>
+      </button>
     {/each}
   </div>
   <button onclick={next}>Next</button>
@@ -59,14 +129,20 @@
   max-height: 600px;
   margin: auto;
   border: 2px solid #333;
+  border-radius: 1rem;
 }
 
 .square {
+  cursor: pointer;
   display: flex;
   align-items: center;
   justify-content: center;
+  font: inherit;
   font-size: 2rem;
+  color: inherit;
   user-select: none;
+  border: none;
+  outline: inherit;
   transition: background ease-in-out 50ms;
 }
 
@@ -84,5 +160,9 @@
 
 .square.dst {
   background: oklch(58.6% 0.253 17.585);
+}
+
+.square.click {
+  background: oklch(76.5% 0.177 163.223);
 }
 </style>
